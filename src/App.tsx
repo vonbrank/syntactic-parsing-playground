2f6502c32b55b0115328e9c38f248d46962dd084
vonbrank <vonbrank@outlook.com>
2023-04-07 00:30:29 +0800

    chore: 添加 material 主题

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import React, { useState } from "react";
 import locales, { LocaleContext } from "../locales/locales";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import { lightTheme, darkTheme } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 const App: React.FC = () => {
     const [localeIndex, setLocaleIndex] = useState(0);
 
     return (
         <LocaleContext.Provider value={locales[localeIndex].locale}>
-            <RouterProvider router={router} />
+            <ThemeProvider theme={lightTheme}>
+                <CssBaseline />
+                <RouterProvider router={router} />
+            </ThemeProvider>
         </LocaleContext.Provider>
     );
 };
