747497f43daf0e7a4b2f7c81f69ec48097027d20
vonbrank <vonbrank@outlook.com>
2023-04-07 10:09:11 +0800

    chore: 引入 scss 并添加 10px 化简

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./reset.css";
+import "./reset.scss";
 import store from "./store";
 import { Provider } from "react-redux";
 
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,17 +1,17 @@
 import { createTheme } from "@mui/material/styles";
 
 export const lightTheme = createTheme({
-    // typography: {
-    //   htmlFontSize: 10,
-    // },
+    typography: {
+      htmlFontSize: 10,
+    },
     palette: {
         mode: "light"
     }
 });
 export const darkTheme = createTheme({
-    // typography: {
-    //   htmlFontSize: 10,
-    // },
+    typography: {
+      htmlFontSize: 10,
+    },
     palette: {
         mode: "dark"
     }
