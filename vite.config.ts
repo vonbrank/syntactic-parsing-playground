eb20c297bd69c782ff6873eb430f65263ee52d68
Ernest Cui <ecuiships@126.com>
2023-04-26 11:14:41 +0800

    chore: reformat

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,13 @@ import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: fileURLToPath(new URL("./src", import.meta.url)),
-      },
-    ],
-  },
-})
+    plugins: [react()],
+    resolve: {
+        alias: [
+            {
+                find: "@",
+                replacement: fileURLToPath(new URL("./src", import.meta.url))
+            }
+        ]
+    }
+});
