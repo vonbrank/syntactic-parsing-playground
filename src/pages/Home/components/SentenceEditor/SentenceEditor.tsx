e01669f827636e080193b7d0f8ceb0d0335a923c
Ernest Cui <ecuiships@126.com>
2023-04-29 11:08:11 +0800

    chore: SentenceEditor Drafted

--- src/pages/Home/components/SentenceEditor/SentenceEditor.tsx
+++ src/pages/Home/components/SentenceEditor/SentenceEditor.tsx
@@ -1,17 +1,71 @@
 import React from "react";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import styles from "./SentenceEditor.module.scss";
+import { splitLines } from "@/modules/utils";
 
 interface SentenceEditorProps {
     isLocked: boolean;
-    code: string;
-    currentIndex: number;
+    sentence: string;
+    lastConsumedIndex: number;
+    onSentenceChange: (sentence: string) => void;
 }
 
 const SentenceEditor: React.FC<SentenceEditorProps> = (
     props: SentenceEditorProps
 ) => {
-    return <div className={styles.divCodeEditorWrapper}></div>;
+    const sentenceLines = splitLines(props.sentence);
+
+    return (
+        <div className={styles.divSentenceEditorWrapper}>
+            <div className={styles.divLineNumberWrapper}>
+                {sentenceLines.map((_, i) => (
+                    <label key={"label" + i} className={styles.lblLineNumber}>
+                        {i + 1}
+                    </label>
+                ))}
+            </div>
+
+            <div className={styles.divSentenceWrapper}>
+                {props.isLocked ? (
+                    <div className={styles.divLockedSentenceWrapper}>
+                        {props.sentence
+                            .substring(0, props.lastConsumedIndex + 1)
+                            .split("")
+                            .map((x, i) => (
+                                <span
+                                    key={`consumed${i}`}
+                                    className={styles.spanConsumedChar}>
+                                    {x}
+                                </span>
+                            ))}
+                        {props.sentence
+                            .substring(props.lastConsumedIndex)
+                            .split("")
+                            .map((x, i) => (
+                                <span
+                                    key={`unconsumed${i}`}
+                                    className={styles.spanUnconsumedChar}>
+                                    {x}
+                                </span>
+                            ))}
+                    </div>
+                ) : (
+                    <textarea
+                        // line-height*lineCount
+                        style={{
+                            height: `${20 * sentenceLines.length}px`
+                        }}
+                        spellCheck={false}
+                        className={styles.taSentence}
+                        value={props.sentence}
+                        onChange={e =>
+                            props.onSentenceChange(e.currentTarget.value)
+                        }
+                    />
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default SentenceEditor;
